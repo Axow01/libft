b58be62c8426d05a0c10e3e24976dfe880422e3f
Mickael Marcotte <mmarcott@c1r5p8.42quebec.com>
2022-10-21 15:09:32 -0400

    ft_isprint

--- libft.h
+++ libft.h
@@ -11,3 +11,5 @@ int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 
 int	ft_isascii(int c);
+
+int	ft_isprint(int c);
--- src/ft_isprint.c
+++ src/ft_isprint.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/15 12:59:34 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/10/21 15:08:23 by mmarcott         ###   ########.fr       */
+/*   Created: 2022/10/21 15:02:49 by mmarcott          #+#    #+#             */
+/*   Updated: 2022/10/21 15:07:54 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdio.h>
-
-int	main(void)
+int	ft_isprint(int c)
 {
-	int b;
-	char bb = '1';
-	b = (int)bb;
-	printf("String lenght: %d\n", ft_strlen("1234"));
-	printf("Is b is alpha: %d\n", ft_isalpha(b));
-	printf("Is 9 digit: %d\n", ft_isdigit((int)'9'));
-	printf("Is 9 alphanum: %d Is b alphanum: %d\n", ft_isalnum((char)'9'), ft_isalnum((char)'b'));
-	printf("Is c printable? %d", ft_isprint(37));
-	
-	return (0);
+	if (c >= 40 && c <= 176)
+		return(1);
+	return(0);
 }
