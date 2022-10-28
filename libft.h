8d5c7f8f9b3b37742a26115e075f6bb1cd6caf09
Mickael Marcotte <mmarcott@c1r5p5.42quebec.com>
2022-10-27 20:58:43 -0400

    fixing libft.h

--- libft.h
+++ libft.h
@@ -1,21 +1,25 @@
-#pragma once
-#include <unistd.h>
-#include <string.h>
+#ifndef LIBFT_H
+# define LIBFT_H
 
-int	ft_strlen(char *str);
+# include <string.h>
+# include <unistd.h>
 
-int	ft_isalpha(int c);
+int		ft_strlen(char *str);
 
-int	ft_isdigit(int c);
+int		ft_isalpha(int c);
 
-int	ft_isalnum(int c);
+int		ft_isdigit(int c);
 
-int	ft_isascii(int c);
+int		ft_isalnum(int c);
 
-int	ft_isprint(int c);
+int		ft_isascii(int c);
+
+int		ft_isprint(int c);
 
 char	ft_toupper(int c);
 
 char	ft_tolower(int c);
 
 char	*ft_strchr(char *s, int c);
+
+#endif
