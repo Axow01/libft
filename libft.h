61ff1d167d691f2cb140f83ac658cad4fd6e1094
Mickael Marcotte <mmarcott@c2r5p2.42quebec.com>
2022-11-10 22:26:31 -0500

    abc

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mick <mick@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 16:11:16 by mmarcott          #+#    #+#             */
-/*   Updated: 2022/11/08 20:12:41 by mick             ###   ########.fr       */
+/*   Updated: 2022/11/10 22:11:24 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,6 +98,8 @@ void				ft_putendl_fd(char *s, int fd);
 
 t_list				*ft_lstnew(void *content);
 
-void 				ft_lstadd_front(t_list **lst, t_list *new);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+
+int					ft_lstsize(t_list *lst);
 
 #endif
--- src/ft_lstsize.c
+++ src/ft_lstsize.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmarcott <mmarcott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 22:19:36 by marvin            #+#    #+#             */
-/*   Updated: 2022/11/10 21:58:38 by mmarcott         ###   ########.fr       */
+/*   Created: 2022/11/10 22:13:19 by mmarcott          #+#    #+#             */
+/*   Updated: 2022/11/10 22:24:47 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+int	ft_lstsize(t_list *lst)
 {
-	if (!lst || !new)
-		return ;
-	new->next = *lst;
-	*lst = new;
+	t_list	*temp;
+	int		size;
+
+	temp = lst;
+	size = 0;
+	while (temp != NULL)
+	{
+		size++;
+		temp = temp->next;
+	}
+	return (size);
 }
